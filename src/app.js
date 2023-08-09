5dd030c192ddef8b17720f80c6460552c26d2b6d
Rodolpheanger <rodolphe.anger@yahoo.fr>
2023-08-09 17:34:12 +0200

    wip

--- src/app.js
+++ src/app.js
@@ -8,15 +8,15 @@ const userRoutes = require("./routes/user");
 const saucesRoutes = require("./routes/sauces");
 
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: "https://p6.rodolpheanger.fr",
-//     methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-//     allowedHeaders:
-//       "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-//     credentials: true, // Inclure les cookies dans les requêtes (si nécessaire)
-//   })
-// );
+app.use(
+  cors({
+    origin: false,
+    methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    allowedHeaders:
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    credentials: true, // Inclure les cookies dans les requêtes (si nécessaire)
+  })
+);
 
 app.use("/images", express.static(path.join(__dirname, "../images")));
 app.use("/api/auth", userRoutes);
